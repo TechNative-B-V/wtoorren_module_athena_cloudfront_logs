1bb6e8f7866eb31405fb772adb216a9647eb1431
Wouter van der Toorren <githubwto@toorren.net>
2023-10-24 13:25:05 +0200

    add query output bucket

--- main.tf
+++ main.tf
@@ -4,32 +4,32 @@ locals {
 
 
 resource "aws_athena_database" "access_logs_athena_database" {
-  count = var.create_database == true ? 1 : 0
-  name = local.athena_db_name_snake_case
+  count  = var.create_database == true ? 1 : 0
+  name   = local.athena_db_name_snake_case
   bucket = var.s3_bucket_name
 }
 
 resource "aws_glue_catalog_table" "cloudfront_logs_catalog_table" {
-  name = var.cloudfront_logs_table_name
+  name          = var.cloudfront_logs_table_name
   database_name = var.create_database == true ? aws_athena_database.access_logs_athena_database[0].name : var.database_name
 
   parameters = {
-    EXTERNAL = "TRUE"
+    EXTERNAL                 = "TRUE"
     "skip.header.line.count" = "2"
   }
 
   storage_descriptor {
-    location = "s3://${var.s3_bucket_name}/${var.cloudfront_access_log_prefix}/"
-    input_format = "org.apache.hadoop.mapred.TextInputFormat"
+    location      = "s3://${var.s3_bucket_name}/${var.cloudfront_access_log_prefix}/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
     output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
 
     ser_de_info {
-      name = "serde"
+      name                  = "serde"
       serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
 
       parameters = {
-        "field.delim": "\t"
-        "serialization.format": "\t"
+        "field.delim" : "\t"
+        "serialization.format" : "\t"
       }
     }
 
@@ -43,11 +43,23 @@ resource "aws_glue_catalog_table" "cloudfront_logs_catalog_table" {
   }
 }
 
-resource "aws_s3_bucket" "athena_query_results" {
-  bucket = "${env}-${var.database_name}-query_output" 
+resource "aws_athena_workgroup" "example" {
+  name = "example"
+
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.athena_query_results.bucket}/output/"
+
+      encryption_configuration {
+        encryption_option = "SSE_KMS"
+        kms_key_arn       = aws_kms_key.example.arn
+      }
+    }
+  }
 }
-
-
 
 # resource "aws_athena_named_query" "cloudfront_logs_saved_query" {
 #   name     = var.queries.*.name
