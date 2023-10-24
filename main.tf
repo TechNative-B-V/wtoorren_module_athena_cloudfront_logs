f58c81d270182a14c365b31d67a5891024423c5e
Wouter van der Toorren <githubwto@toorren.net>
2023-10-24 12:41:04 +0200

    make queries list

--- main.tf
+++ main.tf
@@ -44,8 +44,16 @@ resource "aws_glue_catalog_table" "cloudfront_logs_catalog_table" {
 }
 
 
+# resource "aws_athena_named_query" "cloudfront_logs_saved_query" {
+#   name     = var.queries.*.name
+#   database = var.create_database == true ? aws_athena_database.access_logs_athena_database[0].name : var.database_name
+#   query    = var.queries.*.query
+# }
+
 resource "aws_athena_named_query" "cloudfront_logs_saved_query" {
-  name     = var.queries.*.name
+  count = length(var.queries)
+
+  name     = var.queries[count.index].name
   database = var.create_database == true ? aws_athena_database.access_logs_athena_database[0].name : var.database_name
-  query    = var.queries.*.query
+  query    = var.queries[count.index].query
 }
