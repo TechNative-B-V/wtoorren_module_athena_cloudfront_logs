97730b049c531928b171302a23936e68a7a5ba47
Wouter van der Toorren <githubwto@toorren.net>
2023-10-24 12:34:28 +0200

    make queries list

--- main.tf
+++ main.tf
@@ -47,5 +47,5 @@ resource "aws_glue_catalog_table" "cloudfront_logs_catalog_table" {
 resource "aws_athena_named_query" "cloudfront_logs_saved_query" {
   name     = [var.queries.*.name]
   database = var.create_database == true ? aws_athena_database.access_logs_athena_database[0].name : var.database_name
-  query    = var.queries.*.query
+  query    = [var.queries.*.query]
 }
